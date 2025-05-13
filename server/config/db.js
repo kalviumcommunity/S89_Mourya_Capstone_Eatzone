a5d52537cc48ba8e4165dcb06cb0a509d3b902f7
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-05-13 09:49:18 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-//db connection
-
 export const connectDB = async () =>{
     await mongoose.connect('mongodb+srv://kadalimouryas89:18011801@cluster0.cbk0vyf.mongodb.net/eatzone').then(()=>console.log("DB COnnected"));
 }
