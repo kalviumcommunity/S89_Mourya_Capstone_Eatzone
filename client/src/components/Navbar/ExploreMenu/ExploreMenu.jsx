3a5f952b68700f1108cb53b29c3a7b9d36b29531
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-06-21 17:04:57 +0530

    file

--- client/src/components/Navbar/ExploreMenu/ExploreMenu.jsx
+++ client/src/components/Navbar/ExploreMenu/ExploreMenu.jsx
@@ -3,7 +3,6 @@ import './ExploreMenu.css'
 import { menu_list } from '../../../assets/assets'
 
 const ExploreMenu = ({category,setCategory}) => {
-
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore our menu</h1>
